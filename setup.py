acc81ca319dd1cd083b866c7ee67b6eaa78f7bca
Drew French <rectangletangle@gmail.com>
2015-06-23 18:32:01 -0700

    added pkg stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='imjf',
+    packages=['imjf'],
+    version='0.0.0',
+    description='The official Python client for www.isMyJsFucked.com',
+    author='Drew French',
+    author_email='rectangletangle@gmail.com',
+    url='https://github.com/rectangletangle/imjf-py',
+    install_requires=['requests']
+)
